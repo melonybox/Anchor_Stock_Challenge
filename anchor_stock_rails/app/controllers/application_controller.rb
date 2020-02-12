f21804515e4e62f3e2a20bb5aabcf5ffa64d88bc
melonybox <grandmadcow@gmail.com>
2020-02-11 21:07:25 -0500

    More setup for jwt, routes, models.

--- anchor_stock_rails/app/controllers/application_controller.rb
+++ anchor_stock_rails/app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+  def encode_token(user_id)
+    JWT.encode({user_id: user_id}, "pizzatime")
+  end
+
+  def get_auth_headers
+    request.headers["Authorization"]
+  end
+
+  def decode_token
+    begin
+      JWT.decode(get_auth_headers, "pizzatime")[0]["user_id"]
+    rescue
+      nil
+    end
+  end
+
+  def session_user
+    User.find_by(id: decode_token)
+  end
 end
--- anchor_stock_rails/app/models/user.rb
+++ anchor_stock_rails/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :stocks
+
+  has_secure_password
 end
