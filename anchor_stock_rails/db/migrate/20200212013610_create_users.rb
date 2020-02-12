d9e0fa75fa0c1bbbd1f89daedafdf32cc5d7448c
melonybox <grandmadcow@gmail.com>
2020-02-12 00:22:05 -0500

    hasmany not needed in migration, belongs to is fine, schema made.

--- anchor_stock_rails/db/migrate/20200212013610_create_users.rb
+++ anchor_stock_rails/db/migrate/20200212013610_create_users.rb
@@ -1,7 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.has_many :stocks, foreign_key: true
       t.string :username
       t.string :email
       t.string :password_digest
