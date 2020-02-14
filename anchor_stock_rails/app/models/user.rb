0bd1a35c09707972a626ab783533993040ba17c9
melonybox <grandmadcow@gmail.com>
2020-02-13 23:56:17 -0500

    Added validations in user for presence in username,password,email.

--- anchor_stock_rails/app/models/user.rb
+++ anchor_stock_rails/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  validates :email, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true
+  validates :username, presence: true
+  validates :password, presence: true
   attribute :money_amount, :float, default: 5000.00
 
   has_many :stocks
