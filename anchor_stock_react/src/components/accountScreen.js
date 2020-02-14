3c649dc0c3f7638e3d2ec26023d67718ea180aed
melonybox <grandmadcow@gmail.com>
2020-02-13 21:47:29 -0500

    Form Setup Done

--- anchor_stock_react/src/components/accountScreen.js
+++ anchor_stock_react/src/components/accountScreen.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 class AccountScreen extends React.PureComponent {
   state = {
-    username: "",
-    email: "",
-    password: ""
+    username: '',
+    email: '',
+    password: '',
+    currAccView: 'loginAcc'
   }
 
   handleChange = event => {
@@ -13,14 +14,33 @@ class AccountScreen extends React.PureComponent {
     });
   }
 
+  handleSubmit = event => {
+    event.preventDefault()
+  }
+
+  handleClick = () => {
+    if (this.state.currAccView === 'createAcc'){
+      this.setState({
+        currAccView: 'loginAcc'
+      })
+    } else {
+      this.setState({
+        currAccView: 'createAcc'
+      })
+    }
+  }
+
   render(){
     return(
-      <div id='accountForm'>
-        <form className="centerColumn">
+      <div>
+        <form className="centerColumn" id='accountForm' onSubmit={this.handleSubmit}>
+        {this.state.currAccView === 'createAcc' ?
         <div>
           <label>Username: </label>
           <input type='text' name='username' placeholder='Username' onChange={this.handleChange} />
         </div>
+        :
+        null}
         <div>
           <label>Email: </label>
           <input type='email' name='email' placeholder='Email' onChange={this.handleChange} />
@@ -29,7 +49,16 @@ class AccountScreen extends React.PureComponent {
           <label>Password: </label>
           <input type='password' name='password' placeholder='Password' onChange={this.handleChange} />
         </div>
-        <input type='submit' name='submit' Value='Submit' />
+        {this.state.currAccView === 'createAcc' ?
+        <div>
+          <input type='button' name='button' value='I have an account' onClick={this.handleClick}/>
+          <input type='submit' name='submit' value='Submit' />
+        </div>
+        :
+        <div>
+          <input type='button' name='button' value='Register an account' onClick={this.handleClick}/>
+          <input type='submit' name='submit' value='Submit' />
+        </div>}
         </form>
       </div>
     )
