c61bca3364239b66523938bfe5d02842718b660f
melonybox <grandmadcow@gmail.com>
2020-02-13 20:17:12 -0500

    Initial react setup.

---
+++ anchor_stock_react/src/reducers/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  currentUser: {}
+}
+
+export default function reducer(state = initialState, action) {
+    switch (action.type) {
+      default:
+        return state;
+  }
+}
