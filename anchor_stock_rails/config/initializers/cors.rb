4a0e297126ab74a8a5836bd8552d15a590b069bc
melonybox <grandmadcow@gmail.com>
2020-02-17 06:06:52 -0500

    heroku stuff

--- anchor_stock_rails/config/initializers/cors.rb
+++ anchor_stock_rails/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'fierce-castle-80636.herokuapp.com'
+    origins 'shielded-plateau-63161.herokuapp.com'
 
     resource '*',
       headers: :any,
