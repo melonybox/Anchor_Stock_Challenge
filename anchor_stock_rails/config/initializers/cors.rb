92fe1a3b5defe366484a09fd19f7c44ff942edcc
melonybox <grandmadcow@gmail.com>
2020-02-17 03:24:40 -0500

    cors edit

--- anchor_stock_rails/config/initializers/cors.rb
+++ anchor_stock_rails/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'still-spire-88346.herokuapp.com'
 
     resource '*',
       headers: :any,
